c8348eaddc03868955cc737742b3cbadb5a65d06
changlin93 <767425412@qq.com>
2021-05-04 15:39:51 +0800

    :tada: first commit project

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     rules: {
         'class-methods-use-this': ['off', { exceptMethods: ['constructor'] }], // exceptMethods 传递要忽略其警告的方法名称数组
         'import/no-unresolved': [2, { ignore: ['^@/', '@controller'] }], // @和@controller 是设置的路径别名
-        'no-useless-constructor': 'off',
+        // 'no-useless-constructor': 'off',
         'no-empty-function': ['error', { allow: ['constructors'] }]
     }
 }
